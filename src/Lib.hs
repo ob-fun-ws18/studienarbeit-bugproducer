1c51272f2a7f5514eaed5df9dba10919d0a38572
Mark Tripolt <tripolt@hm.edu>
2018-11-08 13:37:44 +0100

    added travis file

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,13 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
+
+import Data.Ratio
+import Data.Char
+import Data.List
+import Data.Ord
+import Data.Matrix
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+generateMatrix :: Num a => Int -> Int -> Matrix a
+generateMatrix x y = zero x y
