3039370527ee12d3f06a7a556370448ee437d583
Mark Tripolt <tripolt@hm.edu>
2018-11-12 12:58:06 +0100

    added Random import, altered Main function

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,9 @@
 module Main where
 
+import System.IO (hSetBuffering, BufferMode(NoBuffering), stdout)
 import Lib
+import Data.Matrix
+import System.Random
 
 main :: IO ()
-main = someFunc
+main = startGame
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,22 @@
 module Lib where
 
 import Data.Matrix
+import System.Random
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
 generateMatrix :: Num a => Int -> Int -> Matrix a
 generateMatrix x y = zero x y
+
+setStartPoint :: Num a => Matrix a -> Int -> Int -> Matrix a
+setStartPoint matrix x y = r where
+    r = setElem 1 (x, y) matrix
+
+startGame :: IO ()
+startGame = do
+    putStrLn "*** Game started ***"
+    print matrix
+    where
+        empty = generateMatrix 4 4
+        matrix = setStartPoint empty 2 2
