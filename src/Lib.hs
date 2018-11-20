b803a94f0b35727ff01edd4a46fcf369e4515187
Mark Tripolt <tripolt@hm.edu>
2018-11-20 14:19:41 +0100

    added calcNeighbours, changed neighbour functions, added tests

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,6 +3,15 @@ module Lib where
 import Data.Matrix
 import System.Random
 
+data Neighbour = Neighbour Int (Int,Int)
+    deriving(Eq,Show)
+
+getValue :: Neighbour -> Int
+getValue (Neighbour value _ ) = value
+
+getPosition :: Neighbour -> (Int,Int)
+getPosition (Neighbour _ position) = position
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
@@ -17,57 +26,67 @@ setEndPoint :: Matrix Int -> Int -> Int -> Matrix Int
 setEndPoint matrix x y = r where
     r = setElem 16 (x, y) matrix
 
-calcNeighbours :: Matrix Int -> Int -> Int -> Int
+calcNeighbours :: Matrix Int -> Int -> Int -> [Neighbour]
 calcNeighbours matrix x y = r where
-    r = 42
+    upperLeft = upperLeftNeighbour matrix x y
+    upper = upperNeighbour matrix x y
+    upperRight = upperRightNeighbour matrix x y
+    left = leftNeighbour matrix x y
+    right = rightNeighbour matrix x y
+    lowerLeft = lowerLeftNeighbour matrix x y
+    lower = lowerNeighbour matrix x y
+    lowerRight = lowerRightNeighbour matrix x y
+    neighbourList = [upperLeft,upper,upperRight,left,right,lowerLeft,lower,lowerRight]
+    neighbourListFinal = filter (\x -> getValue x /= -1) neighbourList
+    r = neighbourListFinal
 
-upperNeighbour :: Matrix Int -> Int -> Int -> Int
+upperNeighbour :: Matrix Int -> Int -> Int -> Neighbour
 upperNeighbour matrix x y = 
     if x-1 >= 1 
-        then getElem (x-1) y matrix 
-        else -1
+        then Neighbour (getElem (x-1) y matrix) ((x-1),y)
+        else Neighbour (-1) ((x-1),y)
 
-lowerNeighbour :: Matrix Int -> Int -> Int -> Int
+lowerNeighbour :: Matrix Int -> Int -> Int -> Neighbour
 lowerNeighbour matrix x y = 
     if x+1 <= 4 
-        then getElem (x+1) y matrix 
-        else -1
+        then Neighbour (getElem (x+1) y matrix) ((x+1),y) 
+        else Neighbour (-1) ((x+1),y)
 
-leftNeighbour :: Matrix Int -> Int -> Int -> Int
+leftNeighbour :: Matrix Int -> Int -> Int -> Neighbour
 leftNeighbour matrix x y = 
     if y-1 >= 1 
-        then getElem x (y-1) matrix 
-        else -1
+        then Neighbour (getElem x (y-1) matrix) (x,(y-1))
+        else Neighbour (-1) (x,(y-1))
 
-rightNeighbour :: Matrix Int -> Int -> Int -> Int
+rightNeighbour :: Matrix Int -> Int -> Int -> Neighbour
 rightNeighbour matrix x y = 
     if y+1 <= 4 
-        then getElem x (y+1) matrix 
-        else -1
+        then Neighbour (getElem x (y+1) matrix) (x,(y+1)) 
+        else Neighbour (-1) (x,(y+1)) 
 
-upperLeftNeighbour :: Matrix Int -> Int -> Int -> Int
+upperLeftNeighbour :: Matrix Int -> Int -> Int -> Neighbour
 upperLeftNeighbour matrix x y = 
     if x-1 >= 1 && y-1 >= 1 
-        then getElem (x-1) (y-1) matrix 
-        else -1
+        then Neighbour (getElem (x-1) (y-1) matrix) ((x-1),(y-1)) 
+        else Neighbour (-1) ((x-1),(y-1))
 
-upperRightNeighbour :: Matrix Int -> Int -> Int -> Int
+upperRightNeighbour :: Matrix Int -> Int -> Int -> Neighbour
 upperRightNeighbour matrix x y = 
     if x-1 >= 1 && y+1 <= 4 
-        then getElem (x-1) (y+1) matrix 
-        else -1
+        then Neighbour (getElem (x-1) (y+1) matrix) ((x-1),(y+1))
+        else Neighbour (-1) ((x-1),(y+1))
 
-lowerLeftNeighbour :: Matrix Int -> Int -> Int -> Int
+lowerLeftNeighbour :: Matrix Int -> Int -> Int -> Neighbour
 lowerLeftNeighbour matrix x y = 
     if x+1 <= 4 && y-1 >= 1 
-        then getElem (x+1) (y-1) matrix 
-        else -1
+        then Neighbour (getElem (x+1) (y-1) matrix) ((x+1),(y-1)) 
+        else Neighbour (-1) ((x+1),(y-1)) 
 
-lowerRightNeighbour :: Matrix Int -> Int -> Int -> Int
+lowerRightNeighbour :: Matrix Int -> Int -> Int -> Neighbour
 lowerRightNeighbour matrix x y = 
     if x+1 <= 4 && y+1 <= 4 
-        then getElem (x+1) (y+1) matrix 
-        else -1
+        then Neighbour (getElem (x+1) (y+1) matrix) ((x+1),(y+1)) 
+        else Neighbour (-1) ((x+1),(y+1)) 
 
 setPoint :: Matrix Int -> Int -> Int -> Matrix Int
 setPoint matrix x y = r where
--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -12,35 +12,35 @@ testMatrix = matrix 4 4 $ \(i,j) -> j + ((i-1) * 4)
 spec =
     describe "Neighbour Test" $ do
         it "upperLeftNeighbour" $
-            upperLeftNeighbour testMatrix 2 2 `shouldBe` 1
+            upperLeftNeighbour testMatrix 2 2 `shouldBe` Neighbour 1 (1,1)
         it "upperNeighbour" $
-            upperNeighbour testMatrix 2 2 `shouldBe` 2
+            upperNeighbour testMatrix 2 2 `shouldBe`Neighbour 2 (1,2)
         it "upperRightNeighbour" $
-            upperRightNeighbour testMatrix 2 2 `shouldBe` 3
+            upperRightNeighbour testMatrix 2 2 `shouldBe` Neighbour 3 (1,3)
         it "leftNeighbour" $
-            leftNeighbour testMatrix 2 2 `shouldBe` 5
+            leftNeighbour testMatrix 2 2 `shouldBe` Neighbour 5 (2,1)
         it "rightNeighbour" $
-            rightNeighbour testMatrix 2 2 `shouldBe` 7
+            rightNeighbour testMatrix 2 2 `shouldBe` Neighbour 7 (2,3)
         it "lowerLeftNeighbour" $
-            lowerLeftNeighbour testMatrix 2 2 `shouldBe` 9
+            lowerLeftNeighbour testMatrix 2 2 `shouldBe` Neighbour 9 (3,1)
         it "lowerNeighbour" $
-            lowerNeighbour testMatrix 2 2 `shouldBe` 10
+            lowerNeighbour testMatrix 2 2 `shouldBe` Neighbour 10 (3,2)
         it "lowerRightNeighbour" $
-            lowerRightNeighbour testMatrix 2 2 `shouldBe` 11
+            lowerRightNeighbour testMatrix 2 2 `shouldBe` Neighbour 11 (3,3)
         
         it "upperLeftNeighbour -1" $
-            upperLeftNeighbour testMatrix 1 1 `shouldBe` -1
+            upperLeftNeighbour testMatrix 1 1 `shouldBe` Neighbour (-1) (0,0)
         it "upperNeighbour -1" $
-            upperNeighbour testMatrix 1 1 `shouldBe` -1
+            upperNeighbour testMatrix 1 1 `shouldBe` Neighbour (-1) (0,1)
         it "upperRightNeighbour -1" $
-            upperRightNeighbour testMatrix 4 4 `shouldBe` -1
+            upperRightNeighbour testMatrix 4 4 `shouldBe` Neighbour (-1) (3,5)
         it "leftNeighbour -1" $
-            leftNeighbour testMatrix 1 1 `shouldBe` -1
+            leftNeighbour testMatrix 1 1 `shouldBe` Neighbour (-1) (1,0)
         it "rightNeighbour -1" $
-            rightNeighbour testMatrix 4 4 `shouldBe` -1
+            rightNeighbour testMatrix 4 4 `shouldBe` Neighbour (-1) (4,5)
         it "lowerLeftNeighbour -1" $
-            lowerLeftNeighbour testMatrix 1 1 `shouldBe` -1
+            lowerLeftNeighbour testMatrix 1 1 `shouldBe` Neighbour (-1) (2,0)
         it "lowerNeighbour -1" $
-            lowerNeighbour testMatrix 4 4 `shouldBe` -1
+            lowerNeighbour testMatrix 4 4 `shouldBe` Neighbour (-1) (5,4)
         it "lowerRightNeighbour -1" $
-            lowerRightNeighbour testMatrix 4 4 `shouldBe` -1
+            lowerRightNeighbour testMatrix 4 4 `shouldBe` Neighbour (-1) (5,5)
