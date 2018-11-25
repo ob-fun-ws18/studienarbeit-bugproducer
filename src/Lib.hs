fb8300ee57fd4a27b14ac903a70dcb136a2584c9
Mark Tripolt <tripolt@hm.edu>
2018-11-25 11:29:11 +0100

    Renamed Neighbour to Field

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,23 +6,23 @@ import System.Random
 ------------------------------------------------------------
 -- Data
 ------------------------------------------------------------
-data Playground = Playground (Matrix Int) Neighbour
+data Playground = Playground (Matrix Int) Field
     deriving(Eq,Show)
 
 getMatrix :: Playground -> Matrix Int
 getMatrix (Playground matrix _ ) = matrix
 
-getNeighbour :: Playground -> Neighbour
-getNeighbour (Playground _ neighbour) = neighbour
+getField :: Playground -> Field
+getField (Playground _ neighbour) = neighbour
 
-data Neighbour = Neighbour Int (Int,Int)
+data Field = Field Int (Int,Int)
     deriving(Eq,Show)
 
-getValue :: Neighbour -> Int
-getValue (Neighbour value _ ) = value
+getValue :: Field -> Int
+getValue (Field value _ ) = value
 
-getPosition :: Neighbour -> (Int,Int)
-getPosition (Neighbour _ position) = position
+getPosition :: Field -> (Int,Int)
+getPosition (Field _ position) = position
 
 ------------------------------------------------------------
 -- Functions
@@ -33,16 +33,16 @@ generateEmptyMatrix x y = zero x y
 setStartPoint :: Matrix Int -> Int -> Int -> Playground
 setStartPoint matrix x y = r where
     m = setElem 1 (x, y) matrix
-    r = Playground m (Neighbour 1 (x,y))
+    r = Playground m (Field 1 (x,y))
 
 setEndPoint :: Playground -> Int -> Int -> Playground
 setEndPoint playground x y = r where
     matrix = getMatrix playground
     m = setElem 16 (x, y) matrix
-    r = Playground m (Neighbour 16 (x,y))
+    r = Playground m (Field 16 (x,y))
 
     
-calcNeighbours :: Matrix Int -> Int -> Int -> [Neighbour]
+calcNeighbours :: Matrix Int -> Int -> Int -> [Field]
 calcNeighbours matrix x y = r where
     upperLeft = upperLeftNeighbour matrix x y
     upper = upperNeighbour matrix x y
@@ -56,59 +56,59 @@ calcNeighbours matrix x y = r where
     neighbourListFinal = filter (\x -> getValue x /= -1) neighbourList
     r = neighbourListFinal
 
-upperNeighbour :: Matrix Int -> Int -> Int -> Neighbour
+upperNeighbour :: Matrix Int -> Int -> Int -> Field
 upperNeighbour matrix x y = 
     if x-1 >= 1 
-        then Neighbour (getElem (x-1) y matrix) ((x-1),y)
-        else Neighbour (-1) ((x-1),y)
+        then Field (getElem (x-1) y matrix) ((x-1),y)
+        else Field (-1) ((x-1),y)
 
-lowerNeighbour :: Matrix Int -> Int -> Int -> Neighbour
+lowerNeighbour :: Matrix Int -> Int -> Int -> Field
 lowerNeighbour matrix x y = 
     if x+1 <= 4 
-        then Neighbour (getElem (x+1) y matrix) ((x+1),y) 
-        else Neighbour (-1) ((x+1),y)
+        then Field (getElem (x+1) y matrix) ((x+1),y) 
+        else Field (-1) ((x+1),y)
 
-leftNeighbour :: Matrix Int -> Int -> Int -> Neighbour
+leftNeighbour :: Matrix Int -> Int -> Int -> Field
 leftNeighbour matrix x y = 
     if y-1 >= 1 
-        then Neighbour (getElem x (y-1) matrix) (x,(y-1))
-        else Neighbour (-1) (x,(y-1))
+        then Field (getElem x (y-1) matrix) (x,(y-1))
+        else Field (-1) (x,(y-1))
 
-rightNeighbour :: Matrix Int -> Int -> Int -> Neighbour
+rightNeighbour :: Matrix Int -> Int -> Int -> Field
 rightNeighbour matrix x y = 
     if y+1 <= 4 
-        then Neighbour (getElem x (y+1) matrix) (x,(y+1)) 
-        else Neighbour (-1) (x,(y+1)) 
+        then Field (getElem x (y+1) matrix) (x,(y+1)) 
+        else Field (-1) (x,(y+1)) 
 
-upperLeftNeighbour :: Matrix Int -> Int -> Int -> Neighbour
+upperLeftNeighbour :: Matrix Int -> Int -> Int -> Field
 upperLeftNeighbour matrix x y = 
     if x-1 >= 1 && y-1 >= 1 
-        then Neighbour (getElem (x-1) (y-1) matrix) ((x-1),(y-1)) 
-        else Neighbour (-1) ((x-1),(y-1))
+        then Field (getElem (x-1) (y-1) matrix) ((x-1),(y-1)) 
+        else Field (-1) ((x-1),(y-1))
 
-upperRightNeighbour :: Matrix Int -> Int -> Int -> Neighbour
+upperRightNeighbour :: Matrix Int -> Int -> Int -> Field
 upperRightNeighbour matrix x y = 
     if x-1 >= 1 && y+1 <= 4 
-        then Neighbour (getElem (x-1) (y+1) matrix) ((x-1),(y+1))
-        else Neighbour (-1) ((x-1),(y+1))
+        then Field (getElem (x-1) (y+1) matrix) ((x-1),(y+1))
+        else Field (-1) ((x-1),(y+1))
 
-lowerLeftNeighbour :: Matrix Int -> Int -> Int -> Neighbour
+lowerLeftNeighbour :: Matrix Int -> Int -> Int -> Field
 lowerLeftNeighbour matrix x y = 
     if x+1 <= 4 && y-1 >= 1 
-        then Neighbour (getElem (x+1) (y-1) matrix) ((x+1),(y-1)) 
-        else Neighbour (-1) ((x+1),(y-1)) 
+        then Field (getElem (x+1) (y-1) matrix) ((x+1),(y-1)) 
+        else Field (-1) ((x+1),(y-1)) 
 
-lowerRightNeighbour :: Matrix Int -> Int -> Int -> Neighbour
+lowerRightNeighbour :: Matrix Int -> Int -> Int -> Field
 lowerRightNeighbour matrix x y = 
     if x+1 <= 4 && y+1 <= 4 
-        then Neighbour (getElem (x+1) (y+1) matrix) ((x+1),(y+1)) 
-        else Neighbour (-1) ((x+1),(y+1)) 
+        then Field (getElem (x+1) (y+1) matrix) ((x+1),(y+1)) 
+        else Field (-1) ((x+1),(y+1)) 
 
 setPoint :: Playground -> Int -> Int -> Playground
 setPoint playground x y = r where
     matrix = getMatrix playground
     m = setElem 2 (x, y) matrix
-    r = Playground m (Neighbour 2 (x,y))
+    r = Playground m (Field 2 (x,y))
 
 
 ------------------------------------------------------------
